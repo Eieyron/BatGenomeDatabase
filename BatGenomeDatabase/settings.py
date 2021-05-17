bdd28bfdc20c580841c33cbe5583851ff17a157d
avmagnaye <avmagnaye@up.edu.ph>
2021-05-17 23:20:49 +0800

    added information on DB and Strain

--- BatGenomeDatabase/settings.py
+++ BatGenomeDatabase/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Strain',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,17 @@ WSGI_APPLICATION = 'BatGenomeDatabase.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BatGenomeDB',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '1234'
     }
 }
 
---
+++ Strain/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.fields import BooleanField
+
+class Strain(models.Model):
+
+    name = models.CharField(max_length=256)
+    type_strain = models.BooleanField()
+    scientific_name = models.CharField(max_length=256)
+    medium = models.CharField(max_length=256)
+    medium_growth = models.BooleanField()
+    medium_composition = models.CharField(max_length = 256)
+    temperature = models.FloatField()
+    temperature_type = models.CharField(max_length = 256)
+    temperature_range = models.CharField(max_length = 256)
+    # reference_list  =  
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Strain_detail", kwargs={"pk": self.pk})
+
