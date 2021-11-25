3ff3ce4a2a870aab58a001866a94cbd047aea5e6
Aaron Magnaye <avmagnaye@up.edu.ph>
2021-11-25 17:36:39 +0800

    added docker running script

---
+++ docker_run_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# script to run docker images
+
+docker kill $(docker ps -q) # kills all running containers
+docker system prune -a # deletes all stopped containers and images
+
+docker pull avmagnaye/batgenomedatabase:latest
+
+docker run -p 0.0.0.0:8000:8000 -d "avmagnaye/batgenomedatabase"
--- dockerfile
+++ dockerfile
@@ -1,11 +1,11 @@
 # get docker image
 FROM python:3.7
+ENV PYTHONUNBUFFERED=1
 
 # Copy Application files to the image 
 COPY . ./
 
 RUN pip3 install -r requirements
-ENV PYTHONUNBUFFERED=1
 # RUN pip3 install isla-1.8.3-py3-none-any.whl
 # RUN python manage.py migrate
 
