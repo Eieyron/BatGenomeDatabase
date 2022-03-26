a7427f6da5bfd24e3a2172e0f9405792bed35cf7
Aaron Magnaye <avmagnaye@up.edu.ph>
2022-03-26 16:24:14 +0800

    Update settings.py

--- BatGenomeDatabase/settings.py
+++ BatGenomeDatabase/settings.py
@@ -126,6 +126,7 @@ local_development = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'BatGenomeDB',
         'HOST': 'localhost',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': '1234'
     }
